ecd027a26465b38adc90c47be52381e86f5b3f43
eason <1070388550@qq.com>
2019-09-12 21:53:44 +0800

    414

---
+++ src/main/java/com/eason/leetcode/array/leetcode0414/Solution.java
@@ -0,0 +1,32 @@
+package com.eason.leetcode.array.leetcode0414;
+
+import java.net.Socket;
+import java.util.Arrays;
+
+public class Solution {
+    public int thirdMax(int[] nums) {
+        long max1 = Long.MIN_VALUE;
+        long max2 = Long.MIN_VALUE;
+        long max3 = Long.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] > max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            }else if(nums[i] < max1 && nums[i] > max2){
+                max3 = max2;
+                max2 = nums[i];
+            }else if(nums[i] <max2 && nums[i] > max3){
+                max3 = nums[i];
+            }
+        }
+
+        return (int)(max3 == Long.MIN_VALUE ? max1 : max3);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,2,3,1};
+        Solution solution = new Solution();
+        System.out.println(solution.thirdMax(nums));
+    }
+}
